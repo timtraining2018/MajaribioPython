2b32cce344b796544ebde028940f786aa1c12fa8
Tim Ndux <ndungurutimothy@yahoo.com>
2021-01-27 23:04:19 +0300

    add first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+from django.conf import settings
+from django.http import HttpResponse, Http404
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def download(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
